a9be80dd5ca4bd08538b4f15c8f01fdc5a3666dd
Bruno Villasenor <bvillasen@gmail.com>
2015-03-21 15:36:35 -0600

    removed pairs ANTI-opt, life is good again!

--- cudaNbody.cu
+++ cudaNbody.cu
@@ -1,20 +1,6 @@
 #include "vector3D.hT"
 
 
-__device__ double atomicAdd(double* address, double val)
-{
-    unsigned long long int* address_as_ull =
-                                          (unsigned long long int*)address;
-    unsigned long long int old = *address_as_ull, assumed;
-    do {
-        assumed = old;
-        old = atomicCAS(address_as_ull, assumed, 
-                        __double_as_longlong(val + 
-                        __longlong_as_double(assumed)));
-    } while (assumed != old);
-    return __longlong_as_double(old);
-}
-
 extern "C"{
   
 __global__ void main_kernel( const int nParticles, const cudaP Gmass, const int usingAnimation,
@@ -25,13 +11,11 @@ __global__ void main_kernel( const int nParticles, const cudaP Gmass, const int 
   int tid = blockDim.x*blockIdx.x + threadIdx.x;
   
   if (tid < nParticles){
+    
     //Initialize shared array for positions of other particles
     __shared__ cudaP posX_sh[ %(TPB)s ];
     __shared__ cudaP posY_sh[ %(TPB)s ];
     __shared__ cudaP posZ_sh[ %(TPB)s ];
-    __shared__ cudaP accelX_sh[ %(TPB)s ];
-    __shared__ cudaP accelY_sh[ %(TPB)s ];
-    __shared__ cudaP accelZ_sh[ %(TPB)s ];
     
     //Initialize particle position and velocity
     Vector3D pos( posX[tid], posY[tid], posZ[tid] );
@@ -40,17 +24,11 @@ __global__ void main_kernel( const int nParticles, const cudaP Gmass, const int 
     Vector3D posOther, deltaPos;
     cudaP dist;
     int idOther;
-
     for ( int blockNumber=0; blockNumber<gridDim.x; blockNumber++ ){
-      if (blockNumber < blockIdx.x ) continue;
       idOther =  blockNumber*blockDim.x + threadIdx.x;
-      if (idOther < tid ) continue;
       posX_sh[ threadIdx.x ] = posX[ idOther ];
       posY_sh[ threadIdx.x ] = posY[ idOther ];
       posZ_sh[ threadIdx.x ] = posZ[ idOther ];
-      accelX_sh[ threadIdx.x ] = 0;
-      accelY_sh[ threadIdx.x ] = 0;
-      accelZ_sh[ threadIdx.x ] = 0;
       __syncthreads();
       
       idOther = threadIdx.x;
@@ -59,24 +37,12 @@ __global__ void main_kernel( const int nParticles, const cudaP Gmass, const int 
 	posOther.redefine( posX_sh[ idOther ], posY_sh[ idOther ], posZ_sh[ idOther ] );
 	deltaPos = posOther - pos;
 	dist = sqrt( deltaPos.norm2() + epsilon );
-	deltaPos = deltaPos*(1./( dist*dist*dist ) );
+	deltaPos = deltaPos*(1/( dist*dist*dist ) );
 	force += deltaPos;
-	atomicAdd( &( accelX_sh[ threadIdx.x ] ), -deltaPos.x );
-	atomicAdd( &( accelY_sh[ threadIdx.x ] ), -deltaPos.y );
-	atomicAdd( &( accelZ_sh[ threadIdx.x ] ), -deltaPos.z );
 	idOther++;
       }
-      
-      __syncthreads();
-      idOther =  blockNumber*blockDim.x + threadIdx.x;
-      atomicAdd( &( accelZ[ idOther ] ), accelX_sh[ threadIdx.x ] );
-      atomicAdd( &( accelY[ idOther ] ), accelY_sh[ threadIdx.x ] );
-      atomicAdd( &( accelZ[ idOther ] ), accelZ_sh[ threadIdx.x ] );
     }
     force *= Gmass;
-    
-    
-
     
     velX[tid] = vel.x + 0.5*dt*( accelX[tid] + force.x );
     velY[tid] = vel.y + 0.5*dt*( accelY[tid] + force.y );
